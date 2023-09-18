e5c0792b3f41261eb277f29c63de56ff8b23da6c
hamza ali <70458340+hamza-ali313@users.noreply.github.com>
2023-09-18 04:19:54 +0000

    update css

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -5,7 +5,7 @@ const createTodo = async(req,res,next)=>{
   const {text,id} = req.body;
   const todo = new TodoModel({
     id:id,
-    text,// if key and are same we can write single name
+    text,// if key and value are same we can write single name
     completed:false,
     editing:false
   });
